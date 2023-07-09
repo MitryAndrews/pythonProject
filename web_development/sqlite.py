a2e7715e8c08be0f5341dfab87bb5cd266791d19
MitryAndrews <135856596+MitryAndrews@users.noreply.github.com>
2023-07-09 13:22:20 +0400

    2023_07_09

--- web_development/sqlite.py
+++ web_development/sqlite.py
@@ -4,14 +4,22 @@ connection = sql.connect('test.sqlite')
 q = connection.cursor() # с помощью этого курсора мы можем добавлять, получать данные
 
 # Создаем таблицу на языке SQL, описываем желание сделать действия
-q.execute('''CREATE TABLE user (id int auto_increment primary, name varchar, password varchar)''')
+# q.execute('''CREATE TABLE user (id int auto_increment primary key, name varchar, password varchar)''')
 # далее нужно дать команду на производство операции
-connection.commit()
+# connection.commit()
 
 user_name = input('Введите ваше имя: ')
 user_password = input('Введите пароль: ')
 print('Список пользователей: \n')
-q.execute("INSERT INT0 user (name, password) VALUES ('%s', '%s')%(user_name, user_password")
+q.execute("INSERT INTO user (name, password) VALUES ('%s', '%s')"%(user_name, user_password))
+connection.commit()
+q.execute("SELECT * FROM user")
+# если мне просто вывести что-то из БД, то команду commit не нужно делать
+row = q.fetchone() # пользователь в виде списка, где индекс [0] это id индекс [1] имя и [2] пароль
+# с помощью цикла while мы просто что-то делаем с данными пользователя например печатаем
+while row is not None:
+    print('Name ', row[1], '| Password ', row[2])
+    row = q.fetchone()
 
 # Отключение от БД, это как в файлах - открыли файл, то потом нужно закрыть
 # Сначала закрываем курсор, а потом и базу
